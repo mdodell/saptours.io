b60aada59518a2ab426ec3981d76aa69375a4949
mdodell <dodellmitchell@gmail.com>
2019-06-07 16:06:10 -0400

    Got rid of node modules/restructed src folder

---
+++ src/components/App/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'antd';
+
+const App = () => {
+    return (
+        <Button type="primary">Welcome to SAPTours.io!</Button>
+    );
+};
+
+export default App;
